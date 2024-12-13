4a5734bfefcc7b10a385083621dad505f53dea5f
Viktor Dominguez <95234756+viktordmngz@users.noreply.github.com>
2024-12-12 20:49:21 -0500

    Update Sorting Challenge.py

--- Sorting Challenge.py
+++ Sorting Challenge.py
@@ -1,3 +1,18 @@
+'''
+Write a program that will read three numbers and it will sort them in an ascending order and after that will output the sequence.
+
+Input
+2 4 3
+Output
+2 3 4
+
+Input
+6 1 2
+Output
+1 2 6
+
+'''
+
 n = input()
 n = n.split(" ")
 n = [eval(i) for i in n]
