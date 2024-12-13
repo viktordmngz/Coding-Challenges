1ee2b13c3e2c20050e9c4d85388c31ad1bddeff8
Viktor Dominguez <95234756+viktordmngz@users.noreply.github.com>
2024-12-12 20:16:40 -0500

    Update LCM tester.py

--- LCM tester.py
+++ LCM tester.py
@@ -1,3 +1,21 @@
+'''
+Write a function named lcm that given two natural numbers a and b from input.
+Return their LCM in the code block.
+
+The Least Common Multiple (LCM) for two integers a and b is the smallest positive integer that is evenly divisible by both a and b.
+
+In these examples, "Input" refers to the arguments used in the function.
+
+Input
+2 3 
+Output
+6
+
+Input
+4 8 
+Output 
+8
+'''
 def lcm(a, b):
     # Write code here
     if a%b == 0 or b%a == 0:
@@ -6,4 +24,4 @@ def lcm(a, b):
         if a*i%b == 0:
             return a*i
 
-# print(lcm(6, 10))
+print(lcm(2, 3), lcm(4, 8))
