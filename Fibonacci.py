fc066eda13e4b3cddd6a584be37e4c7f6434b252
Viktor Dominguez <95234756+viktordmngz@users.noreply.github.com>
2024-12-12 20:04:13 -0500

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,3 +1,19 @@
+'''
+Create a program that will output every Fibonacci number that is less than or equal to a given number N.
+
+Write a program that gets a natural number N from input and outputs every number from the Fibonacci sequence that is less than or equal to N.
+
+Input
+3
+Output
+1 1 2 3
+
+Input
+6
+Output
+1 1 2 3 5
+'''
+
 # Enter your code here
 n = int(input())
 terms = "1"
@@ -9,6 +25,5 @@ while temp <= n:
     a2 = a1
     a1 = temp
     temp = a1 + a2
-#    print(a1, '\t', a2, '\t', temp)
 
 print(terms)
