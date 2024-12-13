14b24a2415d620e479e1b43122f595604890803e
Viktor Dominguez <95234756+viktordmngz@users.noreply.github.com>
2024-12-12 20:11:54 -0500

    Update Football Standings.py

--- Football Standings.py
+++ Football Standings.py
@@ -1,3 +1,19 @@
+'''
+In the games of football, the winner is awarded 3 points, the loser is not awarded any points and if the match is a draw, both teams get 1 point each. 
+
+Write a program that gets a natural number N from input. In the next N lines there are three numbers W, D, L representing the wins, draws and loses of the specific team respectively.
+Output the number of points that the team with most has.
+
+Input
+3
+4 0 1
+2 0 2
+0 1 4
+
+Output
+12
+'''
+
 # Enter your code here
 n = int(input())
 records = []
