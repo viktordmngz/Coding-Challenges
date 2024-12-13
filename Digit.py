0ab0b7404242fed1114455cda4683965dac4a526
Viktor Dominguez <95234756+viktordmngz@users.noreply.github.com>
2024-12-12 19:38:59 -0500

    Update Digit.py

--- Digit.py
+++ Digit.py
@@ -1,9 +1,27 @@
+'''
+You are given a sequence which begins with 1 and after every element the added sequence increases. 
+But, it starts over as follows: 112123123412345123456
+
+Given a natural number N from input. Return the N-th digit of the given sequence.
+
+Input
+10
+Output
+4
+
+Input
+17
+Output
+2
+
+'''
+
 def digit(n):
     # Write code here
     a = 1
     numbers = ""
     while len(numbers) < n:
-        for i in range(1,a):
+        for i in range(1,a+1):
             numbers += str(i)
         a += 1
-    return numbers[n-1]
+    return int(numbers[n-1])
