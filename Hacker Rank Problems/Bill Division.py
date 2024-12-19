6492a959a8ad54dd1e3923600d205609d37fee86
Viktor Dominguez <95234756+viktordmngz@users.noreply.github.com>
2024-12-18 21:57:43 -0500

    Create Bill Division.py

---
+++ Hacker Rank Problems/Bill Division.py
@@ -0,0 +1,55 @@
+'''
+https://www.hackerrank.com/challenges/bon-appetit/problem?isFullScreen=true
+
+Two friends Anna and Brian, are deciding how to split the bill at a dinner.
+Each will only pay for the items they consume. Brian gets the check and calculates Anna's portion. You must determine if his calculation is correct.
+
+For example, assume the bill has the following prices: bill = [2, 4, 6].
+Anna declines to eat item k=bill[2] which costs 6.
+If Brian calculates the bill correctly, Anna will pay (2+4)/2 = 3.
+If he includes the cost of bill[2], he will calculate (2+4+6)/2 = 6.
+In the second case, he should refund 3 to Anna.
+
+Complete the bonAppetit function in the editor below. It should print Bon Appetit if the bill is fairly split. Otherwise, it should print the integer amount of money that Brian owes Anna.
+
+bonAppetit has the following parameter(s):
+
+* bill: an array of integers representing the cost of each item ordered
+* k: an integer representing the zero-based index of the item Anna doesn't eat
+* b: the amount of money that Anna contributed to the bill
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    # Write your code here
+
+''' REMOVE TO TEST
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
+'''
