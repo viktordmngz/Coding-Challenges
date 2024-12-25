821d8d21b37d524ab720cebcb707065dfd529fc8
Viktor Dominguez <95234756+viktordmngz@users.noreply.github.com>
2024-12-24 23:03:01 -0500

    Update Forming a Magic Square.py

--- Hacker Rank Problems/Forming a Magic Square.py
+++ Hacker Rank Problems/Forming a Magic Square.py
@@ -37,3 +37,39 @@ If we change the bottom right value (s[2][2]) from 5 to 6, the square will be co
 The cost of this exchange is |5-6| which is 1.
 
 '''
+#!/bin/python3
+
+# import math
+# import os
+# import random
+# import re
+# import sys
+
+#
+# Complete the 'formingMagicSquare' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY s as parameter.
+#
+
+def formingMagicSquare(s):
+  # Write your code here
+  row_cost = []
+  col_cost = []
+  
+  for i in range(3):
+    row_cost.append(15-sum(s[i]))
+    col_cost.append(15-sum([s[0][i],s[1][i],s[2][i]]))
+  
+  return sum(row_cost)+sum(col_cost)
+
+'''
+Current issues:
+  Cannot track the changes properly
+  Double counting if there is a change in the row and column
+
+Need a way to track the changes as they are made.
+Maybe make the row changes first, then the columns.
+  Then, check the changes compared to the original array
+
+'''
