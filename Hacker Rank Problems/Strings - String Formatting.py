c907a31b31505adb78242b0a5f2af7662f41c784
Viktor Dominguez <95234756+viktordmngz@users.noreply.github.com>
2025-04-04 10:51:50 -0400

    Add files via upload

---
+++ Hacker Rank Problems/Strings - String Formatting.py
@@ -0,0 +1,64 @@
+'''
+Given an integer, n, print the following values for each integer i from 1 to n:
+
+1. Decimal
+2. Octal
+3. Hexadecimal (capitalized)
+4. Binary
+
+
+FUNCTION DESCRIPTION
+  Complete the print_formatted function in the editor below.
+
+  print_formatted has the following parameters:
+  * int number: the maximum value to print
+
+
+PRINTS
+  The four values must be printed on a single line in the order specified above for each i from 1 to number.
+  Each value should be space-padded to match the width of the binary value of number and the values should be separated by a single space.
+
+
+INPUT FORMAT
+  A single integer denoting n.
+
+
+CONSTRAINTS
+  * 1 <= n <= 99
+
+
+INPUT
+17
+
+OUTPUT
+    1     1     1     1
+    2     2     2    10
+    3     3     3    11
+    4     4     4   100
+    5     5     5   101
+    6     6     6   110
+    7     7     7   111
+    8    10     8  1000
+    9    11     9  1001
+   10    12     A  1010
+   11    13     B  1011
+   12    14     C  1100
+   13    15     D  1101
+   14    16     E  1110
+   15    17     F  1111
+   16    20    10 10000
+   17    21    11 10001
+
+'''
+
+# PROVIDED
+def print_formatted(number):
+    # your code goes here
+    space = len(bin(number)[2:])+1
+    for i in range(1,number+1):
+      print(str(i).rjust(space-1) + oct(i)[2:].rjust(space) + hex(i)[2:].upper().rjust(space) + bin(i)[2:].rjust(space))
+
+# PROVIDED
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
